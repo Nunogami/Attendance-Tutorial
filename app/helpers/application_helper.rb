7efadc1fbdd529eb4c433847958578945ffeb2db
Isao Nunogami <nuno3434@yahoo.co.jp>
2020-03-26 07:27:43 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def full_title(page_name = "")
+    bace_title = "AttendanceApp"
+    if page_name.empty?
+      bace_title
+    else
+      page_name + " | " + bace_title
+    end
+  end
+  
 end
